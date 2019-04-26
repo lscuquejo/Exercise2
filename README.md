20eed825911688b64ffb6dec38eb0b8797b9500b
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-04-26 11:19:27 +0100

    First Try

---
+++ README.md
@@ -0,0 +1,13 @@
+#Links that I check.
+
+##1
+ 
+ https://stackoverflow.com/questions/4211875/check-if-a-string-is-all-caps-in-php
+
+##2
+
+https://php.net/manual/en/function.preg-match.php
+
+##3
+
+https://www.w3schools.com/php/func_array.asp
--- bob.php
+++ bob.php
@@ -3,6 +3,36 @@
 class Bob
 {
 
+    private $answers;
 
+    private function setAnswers()
+    {
+        return $this->answers = array(
+            "question"              =>  "Sure.",
+            "aggressive"            =>  "Whoa, chill out!",
+            "aggressiveQuestion"    =>  "Calm down, I know what I'm doing!",
+            "silence"               =>  "Fine. Be that way!",
+            "anythingElse"          =>  "Whatever.",
+        );
+    }
 
+    public function respondTo($conversation)
+    {
+        $this->setAnswers();
+
+        if (ctype_upper($conversation))
+        {
+            return $this->answers['aggressive'];
+        }
+
+//TODO First attempt just works one test.
+//
+//        if ($conversation === "Tom-ay-to, tom-aaaah-to."){
+//
+//            $response = "Whatever.";
+//
+//            return $response;
+//
+//        }
+    }
 }
