1f36e2d68106379c9be6fa31952d84407bf9c233
leonardo cuquejo soares <leonardo.s@cuquejo.org>
2019-04-30 11:52:42 +0100

    new logic and code update

--- README.md
+++ README.md
@@ -11,3 +11,12 @@ https://php.net/manual/en/function.preg-match.php
 ##3
 
 https://www.w3schools.com/php/func_array.asp
+
+##4
+https://stackoverflow.com/questions/834303/startswith-and-endswith-functions-in-php
+
+##5
+https://php.net/manual/en/function.preg-match.php
+
+##6
+https://wpshout.com/unconditionally-refactoring-nested-statements-cleaner-code/
--- bob.php
+++ bob.php
@@ -5,6 +5,15 @@ class Bob
 
     private $answers;
 
+    private function endsWith($string, $endString)
+    {
+        $len = strlen($endString);
+        if ($len == 0) {
+            return true;
+        }
+        return (substr($string, -$len) === $endString);
+    }
+
     private function setAnswers()
     {
         return $this->answers = array(
@@ -20,10 +29,39 @@ class Bob
     {
         $this->setAnswers();
 
+        /**
+         * Respond to questions.
+         */
+        if ($this->endsWith($conversation, '?') !== false )
+        {
+            return $this->answers['question'];
+        }
+
+        /**
+         * Respond to aggressive conversations without "!"
+         */
         if (ctype_upper($conversation))
         {
             return $this->answers['aggressive'];
         }
+
+        /**
+         * Respond to aggressive conversations with "!".
+         */
+        if ($this->endsWith($conversation, '!') !== false )
+        {
+            return $this->answers['aggressive'];
+        }
+
+        /**
+         * Respond to anyThingElse
+         */
+        return $this->answers['anythingElse'];
+
+//        if (preg_match('/\\\b?\b/', $conversation))
+//        {
+//            return $this->answers['question'];
+//        }
 
 //TODO First attempt just works one test.
 //
